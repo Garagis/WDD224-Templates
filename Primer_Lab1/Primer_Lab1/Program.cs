f5972fd3a1efa714de482e0542972b07693a7b84
Chris Gillis <cgillis93@gmail.com>
2022-08-25 15:03:18 -0400

    Init

---
+++ Primer_Lab1/Primer_Lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Primer_Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
